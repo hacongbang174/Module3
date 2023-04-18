2757b2077028a6be2fba40e949af3daf130531c5
Hà Công Bằng <hacongbang174@gmail.com>
2023-04-18 18:19:23 +0700

    bai2

--- 1. Cơ sở dữ liệu quan hệ/Module3.sql
+++ 1. Cơ sở dữ liệu quan hệ/Module3.sql
@@ -1,4 +1,7 @@
 -- create database student_management
+-- create table student_management.Class (
+-- id INT,
+-- name varchar(255));
 alter table student_management.Class
 Modify id int primary key;
 create table student_management.Teacher (
