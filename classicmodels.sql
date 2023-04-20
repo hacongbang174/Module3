13f7bea37d57dc0745f23ee1852e5ff446b5d2cd
Hà Công Bằng <hacongbang174@gmail.com>
2023-04-20 21:57:45 +0700

    bai5

---
+++ classicmodels.sql
@@ -0,0 +1,29 @@
+--  5. SQL View, Index & Stored Procedure
+-- Thực hành - Chỉ mục trong MySql
+
+SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.'; 
+EXPLAIN SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.'; 
+ALTER TABLE customers ADD INDEX idx_customerName(customerName);
+EXPLAIN SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.'; 
+ALTER TABLE customers ADD INDEX idx_full_name(contactFirstName, contactLastName);
+EXPLAIN SELECT * FROM customers WHERE contactFirstName = 'Jean' or contactFirstName = 'King';
+ALTER TABLE customers DROP INDEX idx_full_name;
+
+-- Thực hành - Store Procedure
+
+DELIMITER //
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+  SELECT * FROM customers;
+END //
+DELIMITER ;
+call findAllCustomers();
+DELIMITER //
+DROP PROCEDURE IF EXISTS `findAllCustomers`//
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+SELECT * FROM customers where customerNumber = 175;
+END //
+DELIMITER ;
+call findAllCustomers();
+
