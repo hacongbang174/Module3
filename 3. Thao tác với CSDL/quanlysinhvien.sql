46dcc17a71f66c4870f5954842914c85eaa6d88c
Hà Công Bằng <hacongbang174@gmail.com>
2023-04-20 21:52:14 +0700

    bai4

--- 3. Thao tác với CSDL/quanlysinhvien.sql
+++ 3. Thao tác với CSDL/quanlysinhvien.sql
@@ -89,3 +89,14 @@ SELECT S.StudentId, S.StudentName, AVG(Mark)
 FROM Student S join Mark M on S.StudentId = M.StudentId
 GROUP BY S.StudentId, S.StudentName
 HAVING AVG(Mark) >= ALL (SELECT AVG(Mark) FROM Mark GROUP BY Mark.StudentId);
+-- Bài tập
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+select * from subject
+where subject.Credit = (SELECT MAX(subject.credit) from subject);
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+select * from subject
+join mark where subject.SubID = mark.subid and mark.Mark = (SELECT MAX(mark.Mark) from mark);
+-- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+select *, (select avg(mark) from student join mark on student.StudentID = mark.StudentID order by mark.StudentID) as avgStudent from student
+where student.StudentID = any (select distinct student.StudentID from student join mark on student.StudentID = mark.StudentID)
+order by student.StudentID;
