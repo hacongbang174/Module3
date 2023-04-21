911a03c668e066677c73475e29d7c55251806c7e
Hà Công Bằng <hacongbang174@gmail.com>
2023-04-21 13:53:19 +0700

    bai5

--- 5. SQL View, Index & Stored Procedure/classicmodels.sql
+++ 5. SQL View, Index & Stored Procedure/classicmodels.sql
@@ -18,6 +18,7 @@ BEGIN
 END //
 DELIMITER ;
 call findAllCustomers();
+
 DELIMITER //
 DROP PROCEDURE IF EXISTS `findAllCustomers`//
 CREATE PROCEDURE findAllCustomers()
@@ -27,3 +28,53 @@ END //
 DELIMITER ;
 call findAllCustomers();
 
+-- Thực hành - luyện tập Store Procedure
+
+DELIMITER //
+CREATE PROCEDURE getCusById(IN cusNum INT(11))
+BEGIN
+  SELECT * FROM customers WHERE customerNumber = cusNum;
+END //
+DELIMITER ;
+call getCusById(175);
+
+DELIMITER //
+create procedure GetCustomersCountByCity( IN in_city VARCHAR(50), OUT total INT)
+begin
+SELECT COUNT(customerNumber) INTO total FROM customers WHERE city = in_city;
+end //
+ DELIMITER ;
+
+CALL GetCustomersCountByCity('Lyon', @total);
+SELECT @total;
+
+
+DELIMITER //
+CREATE PROCEDURE SetCounter(
+    INOUT counter INT,
+    IN inc INT
+)
+BEGIN
+    SET counter = counter + inc;
+END //
+DELIMITER ;
+
+SET @counter = 1;
+
+CALL SetCounter(@counter,1); -- 2
+
+CALL SetCounter(@counter,1); -- 3
+
+CALL SetCounter(@counter,5); -- 8
+
+SELECT @counter; -- 8
+
+CREATE VIEW customer_views AS
+SELECT customerNumber, customerName, phone
+FROM  customers;
+select * from customer_views;
+CREATE OR REPLACE VIEW customer_views AS
+SELEcT customerNumber, customerName, contactFirstName, contactLastName, phone
+FROM customers
+WHERE city = 'Nantes';
+DROP VIEW customer_views;
